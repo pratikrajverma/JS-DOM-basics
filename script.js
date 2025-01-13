3720efc803f26e58fd88769048ff2d48dce061d3
pratikrajverma <erpratikrajverma@gmail.com>
2025-01-13 18:09:30 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,114 @@
+// const headText = document.getElementById('headText');
+// console.log(headText)
+
+// const paraClass = document.getElementsByClassName('paraClass');
+// console.log(paraClass[2])
+
+
+// const headText = document.getElementById('headText');
+
+// console.log(headText.textContent)
+
+
+// const paraClass = document.getElementsByClassName('paraClass');
+// // console.log(paraClass.textContent);
+// for(let i=0;i<paraClass.length;i++){
+//     console.log(paraClass[i].textContent)
+// }
+ 
+
+// const tagName = document.getElementsByTagName('p')
+
+// for(let i=0;i<tagName.length;i++){
+//         console.log(tagName[i].textContent)
+//     }
+    
+
+ 
+
+
+ 
+    
+    // const headText = document.querySelector('#headText')
+    // console.log(headText.textContent)
+    
+    // const paraClass = document.querySelectorAll('p');
+    
+    // for(let i=0;i<paraClass.length;i++){
+    //     console.log(paraClass[i].textContent)
+    // }
+
+
+    // const headText = document.querySelector('#headText');
+
+    // headText.textContent = 'this is head 2'
+
+    // const divHead = document.getElementsByClassName('divHead');
+
+    // console.log(divHead)
+    // divHead[0].innerHTML = '<h1>This is new heading</h1>'
+    
+ 
+
+
+     
+// const paraClass = document.querySelectorAll('.paraClass')
+
+// paraClass[2].textContent = '<h1>This is child heading</h1>'
+// paraClass[2].innerHTML= '<h1>This is child heading</h1>'
+
+ 
+
+
+
+// const headText  = document.querySelector('#headText')
+
+// // headText.textContent = '<h1>This is new head</h1>'
+// headText.innerHTML = '<h1>This is new head</h1>'
+
+// const divHead = document.querySelectorAll('.divHead')
+
+
+// const paraClass = document.querySelectorAll('.paraClass')
+
+// // divHead[0].removeChild(paraClass[0])
+
+// paraClass[2].remove()
+// paraClass[1].remove()
+ 
+
+
+// const para4 = document.createElement('p')
+
+// para4.textContent = 'This is new para 4'
+ 
+// const divHead = document.querySelector('.divHead')
+
+// divHead.appendChild(para4)
+
+// divHead.insertAdjacentElement('beforebegin',para4)
+// divHead.insertAdjacentElement('afterbegin',para4)
+// divHead.insertAdjacentElement('afterend',para4)
+// divHead.insertAdjacentElement('beforeend',para4)
+ 
+
+//  const divHead = document.querySelector('#divHead')
+
+// divHead.style.backgroundColor='aqua';
+// divHead.style.color='blue';
+// divHead.style.height='100px';
+// divHead.style.width='100px';
+// divHead.style.borderRadius = '50px';
+// divHead.style.paddingTop= '20px';
+// divHead.style.boxSizing= 'border-Box';
+// divHead.style.textAlign= 'center';
+
+
+// divHead.style.cssText = 'background-color:red; color:white; height:200px; width:100px;'
+
+
+
+//  const divHead = document.querySelector('#divHead')
+
+
+ 
