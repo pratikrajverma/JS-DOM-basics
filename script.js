46ef67d5c7634bfa0006bb42a660b0f85be040c5
pratikrajverma <erpratikrajverma@gmail.com>
2025-01-15 21:52:10 +0530

    added events

--- script.js
+++ script.js
@@ -1,65 +1,58 @@
 // const headText = document.getElementById('headText');
-// console.log(headText)
+// headText.remove()
+
+// const divHead = document.querySelector('#divHead');
+
+// const lastPara = document.querySelector('#lastPara');
+
+// divHead.removeChild(lastPara)
+
+// console.log(lastPara.textContent)
 
 // const paraClass = document.getElementsByClassName('paraClass');
 // console.log(paraClass[2])
 
-
 // const headText = document.getElementById('headText');
 
 // console.log(headText.textContent)
-
 
 // const paraClass = document.getElementsByClassName('paraClass');
 // // console.log(paraClass.textContent);
 // for(let i=0;i<paraClass.length;i++){
 //     console.log(paraClass[i].textContent)
 // }
- 
 
 // const tagName = document.getElementsByTagName('p')
 
 // for(let i=0;i<tagName.length;i++){
 //         console.log(tagName[i].textContent)
 //     }
-    
 
- 
+// const  headText = document.querySelector('#headText')
+// // console.log(headText.textContent)
+// // console.log(headText.innerHTML)s
 
+// headText.innerHTML = '<h1>this is new head</h1>'
 
- 
-    
-    // const headText = document.querySelector('#headText')
-    // console.log(headText.textContent)
-    
-    // const paraClass = document.querySelectorAll('p');
-    
-    // for(let i=0;i<paraClass.length;i++){
-    //     console.log(paraClass[i].textContent)
-    // }
+// const paraClass = document.querySelectorAll('p');
 
+// for(let i=0;i<paraClass.length;i++){
+//     console.log(paraClass[i].textContent)
+// }
 
-    // const headText = document.querySelector('#headText');
+// const headText = document.querySelector('#headText');
 
-    // headText.textContent = 'this is head 2'
+// headText.textContent = 'this is head 2'
 
-    // const divHead = document.getElementsByClassName('divHead');
+// const divHead = document.getElementsByClassName('divHead');
 
-    // console.log(divHead)
-    // divHead[0].innerHTML = '<h1>This is new heading</h1>'
-    
- 
+// console.log(divHead)
+//  divHead[0].innerHTML = '<h1>This is new heading</h1>'
 
-
-     
 // const paraClass = document.querySelectorAll('.paraClass')
 
 // paraClass[2].textContent = '<h1>This is child heading</h1>'
 // paraClass[2].innerHTML= '<h1>This is child heading</h1>'
-
- 
-
-
 
 // const headText  = document.querySelector('#headText')
 
@@ -68,57 +61,84 @@
 
 // const divHead = document.querySelectorAll('.divHead')
 
-
 // const paraClass = document.querySelectorAll('.paraClass')
 
 // // divHead[0].removeChild(paraClass[0])
 
 // paraClass[2].remove()
 // paraClass[1].remove()
- 
 
+//   const para4 = document.createElement('p')
 
-// const para4 = document.createElement('p')
+//   para4.textContent = 'This is new para 4'
 
-// para4.textContent = 'This is new para 4'
- 
-// const divHead = document.querySelector('.divHead')
+// //   console.log(para4)
 
-// divHead.appendChild(para4)
+// const divHead = document.querySelector('#divHead')
+
+//  divHead.appendChild(para4)
 
 // divHead.insertAdjacentElement('beforebegin',para4)
 // divHead.insertAdjacentElement('afterbegin',para4)
 // divHead.insertAdjacentElement('afterend',para4)
 // divHead.insertAdjacentElement('beforeend',para4)
- 
-
- const divHead = document.querySelector('#divHead')
-
-divHead.style.backgroundColor='aqua';
-divHead.style.color='blue';
-divHead.style.height='100px';
-divHead.style.width='100px';
-divHead.style.borderRadius = '50px';
-// divHead.style.paddingTop= '20px';
-divHead.style.boxSizing= 'border-Box';
-// divHead.style.textAlign= 'center';
-divHead.style.display= 'flex';
-divHead.style.justifyContent= 'center';
-divHead.style.alignItems= 'center';
-
-
-divHead.style.border = '2px solid white';
-divHead.style.boxShadow = '2px 2px 10px red';
-
- 
-
-
-
-// divHead.style.cssText = 'background-color:red; color:white; height:200px; width:100px;'
-
-
 
 //  const divHead = document.querySelector('#divHead')
 
+// divHead.style.backgroundColor='aqua';
+// divHead.style.color='blue';
+// divHead.style.height='100px';
+// divHead.style.width='100px';
+// divHead.style.borderRadius = '50px';
+// // divHead.style.paddingTop= '20px';
+// divHead.style.boxSizing= 'border-Box';
+// // divHead.style.textAlign= 'center';
+// divHead.style.display= 'flex';
+// divHead.style.justifyContent= 'center';
+// divHead.style.alignItems= 'center';
 
- 
+// divHead.style.border = '2px solid white';
+// divHead.style.boxShadow = '2px 2px 10px red';
+
+// divHead.style.cssText = 'background-color:red; color:white; height:200px; width:100px;'
+
+//  const divHead = document.querySelector('#divHead')
+
+// const divHead = document.querySelector('#divHead');
+
+// divHead.style.backgroundColor = 'red';
+// divHead.style.height = '100px';
+// divHead.style.width = '100px';
+// divHead.style.color = 'white';
+// divHead.style.border = '2px solid lime';
+// divHead.style.borderRadius = '50px';
+// divHead.style.textAlign = 'center';
+// divHead.style.fontSize = '10px';
+
+// divHead.style.cssText = 'background-color:red; height:100px; width:100px; border:2px solid yellow;'
+
+// const divHead = document.querySelector('#divHead')
+// divHead.classList.add('active');
+
+// function clickDiv(){
+
+//     const divHead = document.querySelector('#divHead')
+//     divHead.classList.toggle('active');
+
+// }
+
+// const BtnClick = document.querySelector("#BtnClick");
+
+// BtnClick.addEventListener("click", () => {
+//   const divHead = document.querySelector("#divHead");
+//   //   divHead.classList.toggle('active');
+
+//   if (divHead.classList.contains("active")) {
+//     divHead.classList.remove("active");
+//   } else {
+//     divHead.classList.add("active");
+//   }
+// });
+
+
+
